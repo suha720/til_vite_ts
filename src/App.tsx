88fe20011adc40fe9fc32aec62d21cf8b216db78
suha <a50914513@gmail.com>
2025-08-25 11:10:33 +0900

    [docs] 컴포넌트의 이해

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-function App() {
-  const unuse = 1;
-  return <div>App</div>;
-}
+import type React from 'react';
+
+type SampleProps = {
+  children?: React.ReactDOM;
+  age: number;
+  nickName: string;
+};
+const Sample = ({ age, nickName }: SampleProps) => {
+  return (
+    <div>
+      {age} 살이고요, 별명이 {nickName} 임
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+      <Sample age={20} nickName="홍킬동"></Sample>
+    </div>
+  );
+};
 
 export default App;
